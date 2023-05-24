825f71aea229baf3f05694745c53f8cfc2bd75e6
Francisco Diaz <franciscodiazhuerta97@gmail.com>
2023-05-23 19:02:16 -0600

    promesas

---
+++ 09-promise.js
@@ -0,0 +1,13 @@
+const anotherFuncion = () => {
+return new Promise((resolve, reject)=>{
+    if(true){
+        resolve("yea");
+    } else {
+        reject("neeeel");
+    }
+})}
+
+ anotherFuncion().then(response => console.log(response)).catch(err => console.log(err))
+ console.log(anotherFuncion())
+
+// promise algo que puede suceder ahorita jajaja
