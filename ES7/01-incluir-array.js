a3b18e355abc56a7519aa9d4c8f9b8188d422a1d
Francisco Diaz <franciscodiazhuerta97@gmail.com>
2023-05-24 19:53:01 -0600

    ES7

---
+++ ES7/01-incluir-array.js
@@ -0,0 +1,7 @@
+let numbers = [1,2,3,4,5,6,7];
+
+console.log(numbers.includes(4)) //true
+console.log(numbers.includes(8)) //false
+
+// este metodo sirve para encotrar valores en strigs
+// numeros y strigns en arrays se te regresa un valor buleano
